a2a922c6f6cea79c7794e53d9f3eff12fab961e0
Yulissa <8201620961@isise.edu.pe>
2026-04-09 14:58:09 -0500

    primer commit

---
+++ proyectoFinal-Yulissa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace proyectoFinal_Yulissa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
